fac085d392dc1109923fb0b297c11b12da0c05e4
Sheikh Nayan <Sheikh nayansheikhnayan1997@gmail.com>
2020-01-28 19:36:40 +0600

    product detail page

---
+++ domainhostingoffers/product_details.php
@@ -0,0 +1,99 @@
+<?php include 'header.php'; ?>
+<section id="banner-main">
+  <div class="container">
+    <div class="row">
+      <div class="page_heading">
+        <h1>Domain Offers</h1><br>
+
+      </div>
+      <div class="breadcrumb-section">
+        <nav aria-label="breadcrumb">
+           <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Domain offers</li>
+           </ol>
+        </nav>
+
+
+      </div>
+
+
+    </div>
+
+  </div>
+
+
+
+
+</section>
+
+<section class="product_details">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="product-title">
+          <h1>.COM Domain</h1>
+        </div>
+        <div class="product-table">
+          <table class="table table-bordered table-hover table-striped">
+            <tr>
+              <td class="col-sm-3"> <strong>Sub-title</strong></td>
+              <td class="col-sm-9"> .com is a tld</td>
+            </tr>
+            <tr>
+              <td class="col-sm-3"> <strong>Category</strong></td>
+              <td class="col-sm-9"> Domain Offer</td>
+            </tr>
+            <tr>
+              <td class="col-sm-3"> <strong>Provider Name</strong></td>
+              <td class="col-sm-9">Dianahost</td>
+            </tr>
+            <tr>
+              <td class="col-sm-3"> <strong>Start Date</strong></td>
+              <td class="col-sm-9">01-02-2020</td>
+            </tr>
+            <tr>
+              <td class="col-sm-3"> <strong>End Date</strong></td>
+              <td class="col-sm-9">01-03-2020</td>
+            </tr>
+            <tr>
+              <td class="col-sm-3"> <strong>Price</strong></td>
+              <td class="col-sm-9">৳455 TK</td>
+            </tr>
+            <tr>
+              <td class="col-sm-3"> <strong>Offer Note</strong></td>
+              <td class="col-sm-9">Valid for onetime Purchase Only.</td>
+            </tr>
+            <tr>
+              <td class="col-sm-3"> <strong>Description</strong></td>
+              <td class="col-sm-9"><p>This offer is for .COM domain only and its for onetime purchase offer by Dianahost.</p></td>
+            </tr>
+
+          </table>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php';?>
